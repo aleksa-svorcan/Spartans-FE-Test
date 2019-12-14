3c926f8702bd922657692581b58e4ad77773b602
aleksa.svorcan <aleksa.svorcan@gmail.com>
2019-12-14 11:33:50 +0100

    added script file and basic html file

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('client'))
+
+app.get('/', (req, res) => {
+	res.sendfile(
+		path.join(__dirname, '../client/index.html')
+	)
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
